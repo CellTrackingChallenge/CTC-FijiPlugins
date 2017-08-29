a5c898d38476cc69fb327d711c60244e27d2b124
Vladimír Ulman <ulman@mpi-cbg.de>
2017-08-29 15:18:04 +0200

    FIX: organized a bit the 'release' folder.

---
+++ release/docs/CTC/sampleShellCall.sh
@@ -0,0 +1,2 @@
+# example of calling the Technical measures (TRA & SEG) from a Linux/Mac shell command line
+your_path_to_Fiji_binary/ImageJ-macosx --headless --run "Technical measures" "gtPath=\"foo1\",resPath=\"foo2\",calcTRA=true,calcSEG=true"
