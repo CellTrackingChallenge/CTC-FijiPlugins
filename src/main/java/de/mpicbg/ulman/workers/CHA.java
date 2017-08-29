87f6f2ad48bf0f8f242ee8687de123a56ac89485
Vladimír Ulman <ulman@mpi-cbg.de>
2017-08-29 20:16:31 +0200

    ADD: implementation of CHA

--- src/main/java/de/mpicbg/ulman/workers/CHA.java
+++ src/main/java/de/mpicbg/ulman/workers/CHA.java
@@ -13,7 +13,6 @@ import io.scif.img.ImgIOException;
 import java.io.IOException;
 
 import java.util.Vector;
-import java.util.Map;
 import java.util.HashMap;
 
 import de.mpicbg.ulman.workers.ImgQualityDataCache;
@@ -49,6 +48,35 @@ public class CHA
 
 
 	//---------------------------------------------------------------------/
+	/**
+	 * The function returns average FG intensity over all objects found that
+	 * are present at time points (inclusive) \e from till \e to.
+	 * Returns -1 if no object has been found at all.
+	 */
+	private double avgFGfromTimeSpan(final int from, final int to)
+	{
+		//shadows of the/short-cuts to the cache data
+		final Vector<HashMap<Integer,Double>> avgFG = cache.avgFG;
+
+		if (from < 0 || from >= avgFG.size()) return (-1.0);
+		if ( to  < 0 ||  to  >= avgFG.size()) return (-1.0);
+
+		double avg = 0.0;
+		int cnt = 0;
+
+		for (int time = from; time <= to; ++time)
+		{
+			for (Double fg : avgFG.get(time).values())
+			{
+				avg += fg;
+				++cnt;
+			}
+		}
+
+		return (cnt > 0 ? avg/(double)cnt : -1.0);
+	}
+
+
 	/**
 	 * Measure calculation happens in two stages. The first/upper stage does
 	 * data pre-fetch and calculations to populate the TrackDataCache.
@@ -96,31 +124,53 @@ public class CHA
 
 		//shadows of the/short-cuts to the cache data
 		final Vector<HashMap<Integer,Double>> avgFG = cache.avgFG;
-		final Vector<Double> avgBG = cache.avgBG;
-		final Vector<Double> stdBG = cache.stdBG;
 
-		//go over all FG objects and calc their CHAs
-		long noFGs = 0;
-		//over all time points
-		for (int time=0; time < avgFG.size(); ++time)
-		{
-			//over all objects, in fact use their avg intensities
-			for (Double fg : avgFG.get(time).values())
-			{
-				cha += (fg - avgBG.get(time)) / stdBG.get(time);
-				++noFGs;
-			}
-		}
+		//TODO: process pairwise the image sequence and consider only
+		//      pairs where both images have some FG labels, hence
+		//      CHA can be calculated for such pair, and take the average
+		//      from this (and complain if no such pair has been detected)
 
-		//finish the calculation of the average
-		if (noFGs > 0)
+		double a = -1.0, b = -1.0;
+
+		if (avgFG.size() < 2)
 		{
-			cha /= (double)noFGs;
-			log.info("CHA: "+cha);
+			throw new IllegalArgumentException("Cannot calculate CHA from less than two images.");
 		}
 		else
-			log.info("CHA: Couldn't calculate average CHA because there are no cells labelled.");
+		if (avgFG.size() == 2)
+		{
+			a = avgFGfromTimeSpan(0,0);
+			b = avgFGfromTimeSpan(1,1);
+			cha = b - a;
+		}
+		else
+		if (avgFG.size() == 3)
+		{
+			//use largest possible (still overlapping, though) window
+			//windows size = 2 time points
+			a = avgFGfromTimeSpan(0,1);
+			b = avgFGfromTimeSpan(1,2);
+			cha = b - a;
+			cha /= 2.0;
+		}
+		else
+		//if (avgFG.size() > 3)
+		{
+			//use largest possible (possibly overlapping, though) window
+			//windows size = 3 time points
+			final int last = avgFG.size() - 1;
+			a = avgFGfromTimeSpan(0,2);
+			b = avgFGfromTimeSpan(last-2,last);
+			cha = b - a;
+			cha /= (double)last;
+		}
 
+		if (a < 0.0 || b < 0.0)
+			throw new IllegalArgumentException("Current CHA implementation cannot deal "
+				+"with images with no FG labels.");
+
+		log.info("CHA_debug: avg. int. "+a+" -> "+b+", over "+avgFG.size()+" frames");
+		log.info("CHA: "+cha);
 		return (cha);
 	}
 
