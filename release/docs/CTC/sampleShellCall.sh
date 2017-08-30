c296502c8784c3ab883d473bad06fe99fd0e426c
Vladimír Ulman <ulman@mpi-cbg.de>
2017-08-30 21:02:47 +0200

    FIX: sample cmd line call -- all plugin params has to be provided
    it won't work otherwise as it is simply missing values for all parameters,
    and cannot ask for them in the headless mode

--- release/docs/CTC/sampleShellCall.sh
+++ release/docs/CTC/sampleShellCall.sh
@@ -1,2 +1,5 @@
 # example of calling the Technical measures (TRA & SEG) from a Linux/Mac shell command line
-your_path_to_Fiji_binary/ImageJ-macosx --headless --run "Technical measures" "gtPath=\"foo1\",resPath=\"foo2\",calcTRA=true,calcSEG=true"
+your_path_to_Fiji_binary/ImageJ-macosx --headless --run "Technical measures" "resPath=\"foo1\",gtPath=\"foo2\",calcSEG=true,calcTRA=true,pathFooterA=[],pathFooterB=[],measuresHeader=[]"
+
+# example of calling the Dataset measures from a Linux/Mac shell command line
+your_path_to_Fiji_binary/ImageJ-linux64 --headless --run "Dataset measures" "imgPath=\"foo1\",annPath=\"foo2\",xRes=1.0,yRes=1.0,zRes=1.0,calcSNR=true,calcCR=true,calcHeti=true,calcHetb=true,calcRes=true,calcSha=true,calcDen=true,calcCha=true,calcOve=true,calcMit=true,pathFooterA=[],pathFooterB=[],measuresHeader=[]" 2>&1 | grep INFO > `mktemp -u OUTPUT_XXXX.txt`
