05cabec0a50047435213b1f22c9e00327cbb2caf
Vladimir Ulman <ulman@mpi-cbg.de>
2019-12-29 12:11:58 +0100

    CHG: test code with benchmarking
    # Conflicts:
    #	CTC-paper/src/test/java/de/mpicbg/ulman/ctc/testJaccard.java

--- CTC-paper/src/test/java/de/mpicbg/ulman/ctc/testJaccard.java
+++ CTC-paper/src/test/java/de/mpicbg/ulman/ctc/testJaccard.java
@@ -10,28 +10,38 @@ public class testJaccard
 {
 	public static void main(final String... args)
 	{
-		final String fileImgA = "/home/ulman/data/MAC20/02_bestIndiv_CVUT-CZ/mask035.tif";
-		final String fileImgB = "/home/ulman/data/MAC20/02_manAnnot/man_seg035.tif";
+		final String fileImgA = "/home/ulman/data/MAC20/02_bestIndiv_CVUT-CZ/mask035__3D.tif";
+		final String fileImgB = "/home/ulman/data/MAC20/02_manAnnot/man_seg035__3D.tif";
 
 		Img<?> imgA = ImageJFunctions.wrap( new ImagePlus( fileImgA ));
 		Img<?> imgB = ImageJFunctions.wrap( new ImagePlus( fileImgB ));
 
 		// ------ next test ------
+		long a = System.currentTimeMillis();
 		System.out.println("Jaccard   is " + Jaccard.Jaccard(
 			(RandomAccessibleInterval)imgA,881,
 			(RandomAccessibleInterval)imgB,17) );
 
+		long b = System.currentTimeMillis();
 		System.out.println("JaccardLB is " + Jaccard.JaccardLB(
 			(RandomAccessibleInterval)imgA,881,
 			(RandomAccessibleInterval)imgB,17) );
+
+		long c = System.currentTimeMillis();
+		System.out.println("timings: "+(b-a)+", "+(c-b)+" millis");
 
 		// ------ next test ------
+		a = System.currentTimeMillis();
 		System.out.println("Jaccard   is " + Jaccard.Jaccard(
 			(RandomAccessibleInterval)imgA,373,
 			(RandomAccessibleInterval)imgB,3) );
 
+		b = System.currentTimeMillis();
 		System.out.println("JaccardLB is " + Jaccard.JaccardLB(
 			(RandomAccessibleInterval)imgA,373,
 			(RandomAccessibleInterval)imgB,3) );
+
+		c = System.currentTimeMillis();
+		System.out.println("timings: "+(b-a)+", "+(c-b)+" millis");
 	}
 }
